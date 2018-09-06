621d280071e07dfee035e3d945f0d79c1b018543
mario.rangel <mario.rangel@globant.com>
2018-09-06 09:55:52 -0500

    glide to load images

--- presentation/src/main/java/com/globant/mariorangel/marvelcharacters/characters/view/adapter/CharactersAdapter.kt
+++ presentation/src/main/java/com/globant/mariorangel/marvelcharacters/characters/view/adapter/CharactersAdapter.kt
@@ -4,6 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.util.FixedPreloadSizeProvider
 import com.globant.mariorangel.marvelcharacters.R
 import com.globant.mariorangel.models.MarvelCharacter
 import kotlinx.android.synthetic.main.item_row_recycler_character.view.*
@@ -17,9 +21,14 @@ class CharactersAdapter(private val characterList: List<MarvelCharacter>) :
     override fun onBindViewHolder(holder: CharacterHolder, position: Int) = holder.bind(characterList[position])
 
     class CharacterHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val preloadSizeImage = FixedPreloadSizeProvider<ImageView>(91, 123)
+
         fun bind(item: MarvelCharacter) = with(itemView) {
             text_character_name.text = item.name
             text_character_description.text = item.description
+            val imagePath = item.thumbnail!!.path + "." + item.thumbnail!!.extension
+            Glide.with(itemView).load(imagePath).into(image_character)
             itemView.setOnClickListener {  }
         }
     }
